eb2ecbde6e19bd3521b8feccbaef0b4b6f4db862
charleslagger <nvkhoeuet@gmail.com>
2019-01-20 23:03:25 +0700

    MVVM tut

---
+++ HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //this.DataContext = new HelloWorld.ViewModel.StudentViewModel();
+        }
+
+        //private void StudentViewControl_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    HelloWorld.ViewModel.StudentViewModel studentViewModelObject =
+        //       new HelloWorld.ViewModel.StudentViewModel();
+        //    //studentViewModelObject.LoadStudents();
+
+        //    StudentViewControl.DataContext = studentViewModelObject;
+        //}
+    }
+}
---
+++ HelloWorld/Model/StudentModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace HelloWorld.Model
+{
+    public class StudentModel { };
+
+    public class Student : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+
+            set
+            {
+                if (_firstName != value)
+                {
+                    _firstName = value;
+                    RaiseItemChanged("LastName");
+                    RaiseItemChanged("FullName");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+
+            set
+            {
+                if (_lastName != value)
+                {
+                    _lastName = value;
+                    RaiseItemChanged("LastName");
+                    RaiseItemChanged("FullName");
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return _firstName + _lastName;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaiseItemChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+
+}
